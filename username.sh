3c33e83d17328777e72519888f214ea10db2495b
EDang22 <31551043+EDang22@users.noreply.github.com>
2018-11-05 11:12:10 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # username.sh
-echo "Enter a username: \nUsername must be all lowercase letters, digits or the underscore character. Username must start with lowercase letter and be between 3-12 characters."
+echo "Enter a username. Username must be all lowercase letters, digits or the underscore character. Username must start with lowercase letter and be between 3-12 characters."
 read USERNAME
 while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username. Try again"
-	echo "Enter a username: "
+	echo "Enter a username. Username must be all lowercase letters, digits or the underscore character. Username must start with lowercase letter and be between 3-12 characters."
 	read USERNAME
 done
 echo "Thank you"
